cc3d26e30b3ddcb922c07fd96ec6c11c67ff1877
Mark Heckler <mark.heckler@gmail.com>
2021-10-01 16:24:23 -0500

    A few more details

--- src/main/java/com/thehecklers/ownerapi/OwnerApiApplication.java
+++ src/main/java/com/thehecklers/ownerapi/OwnerApiApplication.java
@@ -1,6 +1,9 @@
 package com.thehecklers.ownerapi;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -32,21 +37,27 @@ class OwnerAPIController {
 
 	@GetMapping
 	String greeting() {
-		return "Greetings from the Owner/Customer API app!";
+		return "Greetings from the Owner API app!";
 	}
 
 	@GetMapping("/owners")
-	Flux<String> getOwners() {
+	Flux<OwnerDetails> getOwners() {
 		return client.get()
 				.retrieve()
-				.bodyToFlux(String.class);
+				.bodyToFlux(OwnerDetails.class);
 	}
 
 	@GetMapping("/owner")
-	Mono<String> getOwner() {
+	Mono<OwnerDetails> getOneOwner() {
 		return client.get()
 				.retrieve()
-				.bodyToFlux(String.class)
+				.bodyToFlux(OwnerDetails.class)
 				.next();
 	}
+}
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+class OwnerDetails {
+	private String firstName, lastName, address, city, telephone;
 }
